e0b700e64f8fd1701bf2de6aa4f577e47b1a6a79
Piotr K. Wyrwas <pw.piotr.wyrwas@gmail.com>
2024-09-10 20:55:35 +0200

    Start working on implementing shaders

--- main.lisp
+++ main.lisp
@@ -1,41 +1,50 @@
 (defvar *screen-width* 300)
 (defvar *screen-height* 300)
-(defvar *focal-length* 2)
-(defvar img-stream (open "output.ppm" :direction :output :if-exists :supersede))
-(defvar pixels (make-array
+(defvar *focal-length* 0.1)
+
+(defvar *img-stream* (open "output.ppm" :direction :output :if-exists :supersede))
+(defvar *pixels* (make-array
 	(list *screen-width* *screen-height*)
 	:initial-element (list 0 0 0)))
 
+(defvar *shading-functions* (make-hash-table))
+
+
+;; Image utilities
+
 (defun get-pixel (x y)
-	(aref pixels x y))
+	(aref *pixels* x y))
 
 (defun set-pixel (x y r g b)
-	(setf (aref pixels x y) (list r g b)))
+	(setf (aref *pixels* x y) (list r g b)))
 
 (defun i->xy (ix)
-	(list (mod ix *screen-width*) (floor ix *screen-width*)))
+	(list
+		(mod ix *screen-width*)
+		(floor ix *screen-width*)))
 
 (defun write-image ()
-  	(print "Writing pixels ...")
-  	(write-line "P3" img-stream)
-	(write-line (format nil "~a ~a" *screen-width* *screen-height*) img-stream)
-	(write-line "255" img-stream)
+  	(format t "Writing pixels ...~&")
+  	(write-line "P3" *img-stream*)
+	(write-line (format nil "~a ~a" *screen-width* *screen-height*) *img-stream*)
+	(write-line "255" *img-stream*)
 	(dotimes (ix (* *screen-width* *screen-height*))
 	  	(let*	((xy-pair (i->xy ix))
 			(x (nth 0 xy-pair))
 			(y (nth 1 xy-pair))
 			(px (get-pixel x y)))
-
 			(write-line
 				(format nil "~a ~a ~a"
 					(nth 0 px)
 					(nth 1 px)
-					(nth 2 px)) img-stream)))
-
-	(print "Done."))
+					(nth 2 px)) *img-stream*)))
+	(format t "Done.~&"))
 
 (defun close-stream ()
-	(close img-stream))
+	(close *img-stream*))
+
+
+;; Linear Algebra
 
 (defclass vec3d ()
 	((x	:initform 0
@@ -102,7 +111,7 @@
 			:documentation "The distance along the ray to the intersection point")))
 
 (defmethod ray-vs-shape ((ray ray) (shape sphere))
-	(print "Hello, World!")) ;; TODO Implement the intersection equation here
+	(format t "Hello, World!~&")) ;; TODO Implement the intersection equation here
 
 (defun trace-all-rays ()
 	(dotimes (x *screen-width*)
@@ -113,8 +122,24 @@
 					(+ 128 (floor (* 128 (vec-y direction))))
 					(+ 128 (floor (* 128 (vec-z direction)))))))))
 
+(defun register-shader (id fun)
+	(setf (gethash id *shading-functions*) fun))
+
+(defun retrieve-shader-lambda (id)
+	(gethash id *shading-functions*))
+
+(defun call-shader (id _intersection)
+	(funcall (retrieve-shader-lambda id) _intersection))
+
+(register-shader 'binary-shader
+	(lambda (_intersection)
+		(if _intersection
+			(list 255 255 255)
+		(list 0 0 0))))
+
+(call-shader 'binary-shader nil)
+
+(format t "Rendering ...~&")
 (trace-all-rays)
-
 (write-image)
-
 (close-stream)
